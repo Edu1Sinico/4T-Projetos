337e4f2800714ab26bd56e20af7469a4fbce18c1
Edu1Sinico <EduardoSenai888@gmail.com>
2024-08-01 14:21:42 -0300

    Adicionando algumas coisas para o github

---
+++ IAML-Oracle/Java/exemplo_mnist_redes_neurais/src/main/java/com/example/MnistDataLoader.java
@@ -0,0 +1,8 @@
+package com.example;
+
+import org.nd4j.linalg.dataset.api.iterator.MnistDataSetIterator;
+import org.nd4j.linalg.dataset.api.iterator.impl.MnistDataSetIterator;
+
+public class MnistDataLoader {
+    
+}
