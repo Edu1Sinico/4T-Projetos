154d8f44cc8b053b384ae0d41b2d9c13f26b99c5
Edu1Sinico <EduardoSenai888@gmail.com>
2024-09-20 14:54:18 -0300

    Adicionando o exercício 4 - 20/09

--- Java/conceitosBasicos/src/Fatorial.java
+++ Java/conceitosBasicos/src/Fatorial.java
@@ -1,3 +1,26 @@
+import java.util.Scanner;
+
 public class Fatorial {
-    
+    int n1 = 0;
+
+    Scanner sc = new Scanner(System.in);
+
+    // Método fatorial
+    public int fatorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * fatorial(n - 1);
+        }
+    }
+
+    public void calcularFatorial() {
+        try {
+            System.out.println("\nInforme um valor para ser calculado o seu fatorial: ");
+            n1 = Integer.parseInt(sc.nextLine());
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("Por favor, informe apenas números inteiros positivos.");
+        }
+        System.out.println("\nResultado do fatorial de " + n1 + ": " + fatorial(n1));
+    }
 }
